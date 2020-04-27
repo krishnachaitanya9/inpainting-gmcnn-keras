f8c6f0c709dc7fc66181b3a0c4dd82f874e7a2f7
Krishna <chaitanyaradon89@gmail.com>
2020-04-26 23:57:28 -0600

    Update training script

--- run_training.sh
+++ run_training.sh
@@ -10,5 +10,12 @@
 #    └── celeb
 # Inside the mask there should all masks in jpg/png formats
 # Inside celeb folders there should all be images in jpg/png formats
-python runner.py --train_path "$1/train" --test_path "$1/test" --mask_path "$1/mask" --experiment_name dl_project -warm_up_generator
-python runner.py --train_path "$1/train" --test_path "$1/test" --mask_path "$1/mask" --experiment_name dl_project -from_weights
+warm_up=false
+if [ ! $warm_up ]
+then
+  python runner.py --train_path "$1/train" --test_path "$1/test" --mask_path "$1/mask" --experiment_name dl_project -warm_up_generator
+fi
+for i in {1..5}
+do
+  python runner.py --train_path "$1/train" --test_path "$1/test" --mask_path "$1/mask" --experiment_name dl_project -from_weights
+done
