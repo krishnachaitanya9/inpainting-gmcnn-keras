26910c3612f3d451b397e212f1f7492fa8e96194
Krishna <chaitanyaradon89@gmail.com>
2020-05-07 04:15:50 -0600

    Send to VM

--- command.sh
+++ command.sh
@@ -1,2 +1,2 @@
 # Use this command to run continuous 8 hrs training for models
-python runner.py --train_path "$inpainting/train" --test_path "$inpainting/test" --mask_path "$inpainting/mask" --experiment_name dl_project -warm_up_generator && python runner.py --train_path "$inpainting/train" --test_path "$inpainting/test" --mask_path "$inpainting/mask" --experiment_name dl_project -from_weights
+python runner.py --train_path "$inpainting/train" --mask_path "$inpainting/mask" --experiment_name dl_project -warm_up_generator && python runner.py --train_path "$inpainting/train" --mask_path "$inpainting/mask" --experiment_name dl_project -from_weights
